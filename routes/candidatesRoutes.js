7364bf69cf4c4b40c1eba7c619025fed56dbb263
MD Rahi <fhrstation21@gmail.com>
2024-03-19 16:49:33 +0600

    change percentage for gender

--- routes/candidatesRoutes.js
+++ routes/candidatesRoutes.js
@@ -38,11 +38,24 @@ router.get('/:status', async (req, res) => {
 router.get('/gender/compare', async (req, res) => {
     try {
         const candidatesByGender = await getCandidatesByGender();
-        res.json(candidatesByGender);
+        
+        const totalCandidates = candidatesByGender.female + candidatesByGender.male;
+
+        const femalePercentage = (candidatesByGender.female / totalCandidates) * 100;
+
+        const malePercentage = (candidatesByGender.male / totalCandidates) * 100;
+
+        const percentages = {
+            "female": femalePercentage,
+            "male": malePercentage
+        };
+
+        res.json(percentages);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
 
 export default router;
